5cd65d811aa0899045fcb7668eb6c9422bdb1701
Daniel Magro <dando385@yahoo.com>
2023-09-11 21:22:51 -0400

    walking through shuffle loop

--- script.js
+++ script.js
@@ -20,14 +20,16 @@ function shuffleDeck(deck) {
     for (let i = 0; i < deck.length; i++) {
         // pick a random card from the deck
         const j = Math.floor(Math.random() * (i + 1));
-        
+        console.log(j);
         // swap the current card with the random card using a temporary variable
         const temp = deck[i];
+        console.log(deck);
         deck[i] = deck[j];
+        console.log(deck);
         deck[j] = temp;
+        console.log(deck);
     }
 }
-
 
 // shuffle the deck
 shuffleDeck(deck);
