c1e52cd4fc867653bb3c47a06fd4373908a6d27e
Daniel Magro <dando385@yahoo.com>
2023-09-11 21:30:16 -0400

    commenting out shuffle elements

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let deck = [];
 
+window.onload = CreateDeck;
+window.onload = ShuffleDeck;
+
+
 // create a deck of cards
 function CreateDeck() {
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10','J', 'Q', 'K', 'A'];
@@ -16,23 +20,23 @@ CreateDeck();
 
 console.log(deck);
 
-function shuffleDeck(deck) {
+function ShuffleDeck(deck) {
     for (let i = 0; i < deck.length; i++) {
         // pick a random card from the deck
         const j = Math.floor(Math.random() * (i + 1));
         console.log(j);
         // swap the current card with the random card using a temporary variable
         const temp = deck[i];
-        console.log(deck);
+        // console.log(deck);
         deck[i] = deck[j];
-        console.log(deck);
+        // console.log(deck);
         deck[j] = temp;
-        console.log(deck);
+        // console.log(deck);
     }
 }
 
 // shuffle the deck
-shuffleDeck(deck);
+ShuffleDeck(deck);
 
 console.log(deck);
 
