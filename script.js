5910e32550810fef37f60b52a84b02d3b87b061d
Daniel Magro <dando385@yahoo.com>
2023-09-13 21:56:44 -0400

    chrome not loading consolelogs

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 let deck = [];
-
-window.onload = CreateDeck;
-window.onload = ShuffleDeck;
-
-
 // create a deck of cards
 function CreateDeck() {
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10','J', 'Q', 'K', 'A'];
@@ -22,7 +17,7 @@ console.log(deck);
 
 function ShuffleDeck(deck) {
     for (let i=52; i < Deck.length; i++) {
-        const j = Math.floor(Math.random() * deck.length-i);
+        const j = Math.floor(Math.random() * deck.length);
         console.log(j);
         console.log(deck);
         const temp = deck[i];  
@@ -32,7 +27,7 @@ function ShuffleDeck(deck) {
 }
 
 // shuffle the deck
-ShuffleDeck(deck);
+ShuffleDeck(deck)
 
 console.log(deck);
 
