b9f5019c56f04fc91761f7e8bf60942db7383fd7
Daniel Magro <dando385@yahoo.com>
2023-09-15 09:14:03 -0400

    adding comments to js

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ let playerHand = [];
 
 let gameOver = false;
 
-
-
-
-
-
 function createDeck() {
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10','J', 'Q', 'K', 'A'];
     const suits = ['C', 'D', 'H', 'S'];
@@ -25,6 +20,7 @@ function createDeck() {
     }
 }
 
+//run CreateDeck function
 createDeck();
 
 console.log(deck);
@@ -32,8 +28,6 @@ console.log(deck);
 function shuffleDeck(deck) {
     for (let i=0; i < deck.length; i++) {
         const j = Math.floor(Math.random() * deck.length);
-        console.log(j);
-        console.log(deck);
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
 }
@@ -41,21 +35,21 @@ function shuffleDeck(deck) {
 // shuffle the deck
 shuffleDeck(deck)
 
-console.log(deck);
-
-// not sure if deck needs to be passed in as an argument
+// create function to deal from the deck
 function dealCard(deck) {
     const card = deck.pop;
     return card;
 }
 
+// create function to deal initial hand
 function initialDeal() {
-    for (let i=0; i < 2; i++) {
+    for (let i=0; i<2; i++) {
         dealerHand.push(dealCard(deck));
         playerHand.push(dealCard(deck));
     }
     updateGame()
 }
+
 
 function updateGame() {
     //update player's hand
@@ -66,32 +60,32 @@ function updateGame() {
             cardImage.src = `assets/images/cards/${card}.png`;
             playerCardsElement.appendChild(cardImage);
         }
-    
+    //update dealer's hand
     const dealerCardsElement = document.getElementById('dealer-cards');
         dealerCardsElement.innerHTML = ''; 
         for (const card of dealerHand) {
             const cardImage = document.createElement('div');
             cardImage.src = `assets/images/cards/${card}.png`;
             dealerCardsElement.appendChild(cardImage);
-
-
         }   
-}
-
-for (const card of hand) {
-    const value = card.charAt(0);
-    if (value === 'A') {
+    //assign values to cards    
+    for (const card of hand) {
+        const value = card.charAt(0);
+        if (value === 'A') {
         aceCount++;
         score += 11; // Assume Ace as 11 initially
-    } else if (value === 'K' || value === 'Q' || value === 'J') {
+        } else if (value === 'K' || value === 'Q' || value === 'J') {
         score += 10; // Face cards are worth 10 points
-    } else {
+        } else {
         score += parseInt(value); // Other cards are worth their face value
+        }
+    }   
+
+    // If score>21 and there are aces, adjust ace value(s) to 1 from 11
+    while (score > 21 && aceCount > 0) {
+        score -= 10; // Converts 11 to 1 by subtracting 10 from the score
+        aceCount--;
     }
-}
 
-// If the score is greater than 21 and there are aces, set some/all of them to 1
-
-while (aceCount>0 && score>21) {
-    
+    if
 }
